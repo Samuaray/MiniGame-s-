5780b39260cf1ce04dbc0a8de4c55e587f25a839
Samuel Araya <saraya95@yahoo.com>
2022-10-28 20:32:46 -0700

    first push

---
+++ RockPaperSissors/src/MainGame/mainGame.java
@@ -0,0 +1,14 @@
+public class mainGame
+{
+    public void gameStart()
+    {
+        
+        
+
+    }
+
+
+
+
+
+}
---
+++ RockPaperSissors/src/MainGame/player.java
@@ -0,0 +1,34 @@
+public class player
+{
+    private String name;
+    private int score;
+    private boolean status;
+    private String[] gameMoves = {"Rock", "Paper", "Sissors"};
+    
+    public void gameStart()
+    {
+        int score = 0;
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
