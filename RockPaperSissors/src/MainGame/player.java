795e37a397df42e5a4b13ff891000f57d1ad3d1c
Samuel Araya <saraya95@yahoo.com>
2022-11-01 18:35:08 -0700

    added the foundation of gameCheck method which will determine who wins.

--- RockPaperSissors/src/MainGame/player.java
+++ RockPaperSissors/src/MainGame/player.java
@@ -13,6 +13,66 @@ public class player
         this.chosenMove = chosenMove;
     }
     
+    public  gameCheck(String chosenMove1, String chosenMove2) //get rid of void after you finish setting this up.
+    {
+        if (chosenMove1.equals("Rock"))
+        {
+            if(chosenMove2.equals("Rock"))
+            {
+                return //tie
+            }
+            else if(chosenMove2.equals("Paper"))
+            {
+                return //1 loses
+            }
+            else if(chosenMove2.equals("Sissors"))
+            {
+                return //1 wins
+            }
+            else
+            {
+                return "error";
+            }
+        }
+        if (chosenMove1.equals("Paper"))
+        {
+            if(chosenMove2.equals("Rock"))
+            {
+                return //1wins
+            }
+            else if(chosenMove2.equals("Paper"))
+            {
+                return //tie
+            }
+            else if(chosenMove2.equals("Sissors"))
+            {
+                return //1 loses
+            }
+            else
+            {
+                return "error";
+            }
+        }
+        if (chosenMove1.equals("Sissors"))
+        {
+            if(chosenMove2.equals("Rock"))
+            {
+                return //1 loses
+            }
+            else if(chosenMove2.equals("Paper"))
+            {
+                return //1 wins
+            }
+            else if(chosenMove2.equals("Sissors"))
+            {
+                return //tie
+            }
+            else
+            {
+                return "error";
+            }
+        }
+    }   
 
 
 
